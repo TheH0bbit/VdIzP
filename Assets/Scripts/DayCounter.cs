135f62841b738673204957b6127894f9a3c1a747
Leonie <leonie@wargitsch.de>
2021-06-25 11:55:59 +0200

    Changes yay

---
+++ Assets/Scripts/DayCounter.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DayCounter : MonoBehaviour
+{
+
+    [SerializeField] GameObject textObject;
+    private TextMeshProUGUI textMeshProUGUI;
+
+    [SerializeField] BooleanVariable playedMinigame;
+    [SerializeField] IntVariable treeLevel;
+
+    private int currentDay = 0;
+
+    private void Start() 
+    {
+        textMeshProUGUI = textObject.GetComponent<TextMeshProUGUI>();
+        Debug.Log(textMeshProUGUI.text);
+        playedMinigame.SetValue(false);
+    }
+
+    public void NextDay()
+    {
+        if(currentDay == 6)
+        {
+            currentDay = 0;
+            treeLevel.SetValue(0);
+            Debug.Log("Reached End of Week");
+        } else
+        {
+            currentDay++;
+        }
+        switch(currentDay)
+        {
+            case 1:
+                textMeshProUGUI.text = "DIENSTAG";
+                break;
+            case 2:
+                textMeshProUGUI.text = "MITTWOCH";
+                break;
+            case 3:
+                textMeshProUGUI.text = "DONNERSTAG";
+                break;
+            case 4:
+                textMeshProUGUI.text = "FREITAG";
+                break;
+            case 5:
+                textMeshProUGUI.text = "SAMSTAG";
+                break;
+            case 6:
+                textMeshProUGUI.text = "SONNTAG";
+                break;
+            case 0:
+                textMeshProUGUI.text = "MONTAG";
+                break;
+            default:
+                Debug.Log("Sth went wrong with the Day Counter");
+                break;
+        }
+            Debug.Log("Current Day of the week: " + currentDay);
+            playedMinigame.SetValue(false);
+    }
+
+    public void DoMinigame()
+    {
+        Debug.Log("Played a Minigame");
+        if(playedMinigame.GetValue() == false)
+        {
+            Debug.Log("Played first Minigame of the day");
+            playedMinigame.SetValue(true);
+            treeLevel.SetValue(treeLevel.GetValue() + 1);
+        }
+    }
+}
---
+++ Assets/Scripts/Tree.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tree : MonoBehaviour
+{
+
+    [SerializeField] float flowerThreshhold = 1f;
+    [SerializeField] FloatVariable RechenStats;
+    private float oldRechenStats;
+    private int rechenFlowers = 0;
+
+    [SerializeField] FloatVariable BrainPower;
+    private float oldBrainPower;
+    private int brainFlowers = 0;
+
+   [SerializeField] GameObject [] FlowersBlue;
+   [SerializeField] GameObject [] FlowersRed;
+
+
+    private void Start() {
+        for(int i = 0; i < FlowersBlue.Length; i++)
+        {
+            FlowersBlue[i].GetComponent<SpriteRenderer>().enabled = false;
+        }
+        for(int i = 0; i < FlowersRed.Length; i++)
+        {
+            FlowersRed[i].GetComponent<SpriteRenderer>().enabled = false;
+        }
+        RechenStats.SetValue(0);
+        BrainPower.SetValue(0);
+        oldRechenStats = 0f;
+        oldBrainPower = 0f;
+    }
+
+   private void Update() {
+       CheckStats(oldRechenStats, RechenStats.GetValue(), rechenFlowers, Stat.Rechen);
+       CheckStats(oldBrainPower, BrainPower.GetValue(), brainFlowers, Stat.Brainpower);
+
+       oldRechenStats = RechenStats.GetValue();
+       oldBrainPower = BrainPower.GetValue();
+   }
+
+    private void CheckStats(float oldStats, float newStats, int flowers, Stat statType) 
+    {
+        if (oldStats != newStats)
+        {
+            if(newStats % flowerThreshhold > flowers)
+            {
+                flowers = (int)(newStats % flowerThreshhold);
+                SpawnFlower(statType, flowers);
+            }
+        }
+    }
+    private void SpawnFlower(Stat statType, int noFlowers)
+    {
+        switch (statType)
+        {
+            case Stat.Rechen:
+                for(int i = 0; i < noFlowers && i < FlowersBlue.Length; i++)
+                {
+                    FlowersBlue[i].GetComponent<SpriteRenderer>().enabled = true;
+                }
+                break;
+            case Stat.Brainpower:
+            for(int i = 0; i < noFlowers && i < FlowersBlue.Length; i++)
+                {
+                    FlowersRed[i].GetComponent<SpriteRenderer>().enabled = true;
+                }
+                break;
+            default:
+                break;
+        }
+    }
+}
