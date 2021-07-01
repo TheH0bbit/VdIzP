39c1cea1513761a93e843f2ed9a21fa6adcfdd62
Raphael P <raphaelpenz135@gmail.com>
2021-07-01 14:00:56 +0200

    Tree/FlowerStorage implemented,

--- Assets/Scripts/TreeCode/TreeController.cs
+++ Assets/Scripts/TreeCode/TreeController.cs
@@ -11,9 +11,16 @@ public class TreeController : MonoBehaviour
         
     }
 
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+    static void OnBeforeSceneLoadRuntimeMethod()
+    {
+        PlayerPrefs.DeleteAll();
+    }
+
     public void SetTreeValues(float bp, float rp, int treeID)
     {
         trees[treeID].GetComponent<TreeHandler>().SetTreeStats(bp, rp);
+        trees[treeID].GetComponent<TreeHandler>().CreateFlowers();
     }
 
     // Update is called once per frame
@@ -22,3 +29,4 @@ public class TreeController : MonoBehaviour
         
     }
 }
+
--- Assets/Scripts/TreeCode/TreeHandler.cs
+++ Assets/Scripts/TreeCode/TreeHandler.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class TreeHandler : MonoBehaviour
 {
-    public Sprite[] spriteArray;
+    public GameObject[] redFlowerArray;
+    public GameObject[] blueFlowerArray;
     private SpriteRenderer spriteRenderer;
     public int TreeID;
     [SerializeField] IntVariable CurrTree;
@@ -13,7 +14,7 @@ public class TreeHandler : MonoBehaviour
 
     public float Stat1Stored;
     public float Stat2Stored;
-    private bool initiated;
+    [SerializeField] private bool initiated;
 
     [SerializeField] private CanvasGroup CG;
 
@@ -24,12 +25,35 @@ public class TreeHandler : MonoBehaviour
         CG = GameObject.Find("StatsCanvas").GetComponent<CanvasGroup>();
         CG.alpha = 0;
         initiated = false;
-        spriteRenderer.enabled = false;
+        //spriteRenderer.enabled = false;
+        InitFlowers();
+        CreateFlowers();
     }
 
     // Update is called once per frame
     void Update()
     {
+    }
+
+    void OnDisable()
+    {
+        PlayerPrefs.SetFloat("Stat1" + TreeID, Stat1Stored);
+        PlayerPrefs.SetFloat("Stat2" + TreeID, Stat2Stored);
+        if(initiated)
+            PlayerPrefs.SetInt("Init" + TreeID, 1);
+        else
+            PlayerPrefs.SetInt("Init" + TreeID, 0);
+    }
+
+    void OnEnable()
+    {
+        Stat1Stored = PlayerPrefs.GetFloat("Stat1" + TreeID);
+        Stat2Stored = PlayerPrefs.GetFloat("Stat2" + TreeID);
+        int test = PlayerPrefs.GetInt("Init" + TreeID);
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+        spriteRenderer.enabled = true;
+        CreateFlowers();
+        
     }
 
     public void SetTreeStats(float bp, float rp)
@@ -42,13 +66,13 @@ public class TreeHandler : MonoBehaviour
             spriteRenderer.enabled = true;
             initiated = true;
         }
-
     }
+
 
     private void OnMouseEnter()
     {
-        Stat1.SetValue(Stat1Stored);
-        Stat2.SetValue(Stat2Stored);
+        Stat1.SetValue(Stat1Stored*0.05f);
+        Stat2.SetValue(Stat2Stored*0.05f);
         CG.alpha = 1;
         CG.interactable = true;
         CG.blocksRaycasts = true;
@@ -61,8 +85,34 @@ public class TreeHandler : MonoBehaviour
         CG.blocksRaycasts = false;
     }
 
+
+    public void CreateFlowers()
+    {
+        for(int i=0; i < Mathf.Clamp((int)Mathf.Round(Stat2Stored), 0, 3); i++)
+        {
+            redFlowerArray[i].GetComponent<SpriteRenderer>().enabled = true;
+        }
+        for (int i = 0; i < Mathf.Clamp((int)Mathf.Round(Stat1Stored), 0, 3); i++)
+        {
+            blueFlowerArray[i].GetComponent<SpriteRenderer>().enabled = true;
+        }
+    }
+
+    private void InitFlowers()
+    {
+        for (int i = 0; i < redFlowerArray.Length; i++)
+        {
+            redFlowerArray[i].GetComponent<SpriteRenderer>().enabled = false;
+        }
+        for (int i = 0; i < blueFlowerArray.Length; i++)
+        {
+            blueFlowerArray[i].GetComponent<SpriteRenderer>().enabled = false;
+        }
+    }
+
+
     void ChangeSprite(int sprite)
     {
-        spriteRenderer.sprite = spriteArray[sprite];
+       // spriteRenderer.sprite = spriteArray[sprite];
     }
 }
